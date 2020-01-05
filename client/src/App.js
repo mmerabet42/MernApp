bf9ddd5e8576269f332da8e952bd50c443b900c2
Mohammed MERABET <mmerabet@student.42.fr>
2020-01-05 02:25:56 +0100

    Front-end

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Container, Button } from 'reactstrap';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <input></input>
+        <input></input>
+        <button color="primary">Login</button>
+      </div>
+    );
+  }
+};
+
+export default App;
